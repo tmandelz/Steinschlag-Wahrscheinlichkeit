813106f2c3608ce9fb00f5d5b7b1f809fbbd0539
Thomas Mandelz <tmandelz@hotmail.com>
2021-10-28 13:43:52 +0200

    Loops

--- goodLoops.py
+++ goodLoops.py
@@ -7,7 +7,7 @@ Created on Thu Oct 28 09:26:32 2021
 
 import matplotlib
 import numpy as np
-from scipy.stats import expon 
+from scipy.stats import expon
 import pandas as pd
 from statistics import mean, median
 from numpy.random import normal
@@ -15,53 +15,58 @@ from matplotlib import pyplot
 from numpy.random.mtrand import exponential
 from scipy import stats
 
+
 def FindmostfittingDistribution(MatrixColumn):
 
-	## List of available Distributions for fitting in scipy
-	list_of_dists = ['alpha','anglit','arcsine','beta','betaprime','bradford','burr','burr12','cauchy','chi','chi2','cosine','dgamma','dweibull','expon','exponnorm','exponweib','exponpow','f','fatiguelife','fisk','foldcauchy','foldnorm','genlogistic','genpareto','gennorm','genexpon','genextreme','gausshyper','gamma','gengamma','genhalflogistic','gilbrat','gompertz','gumbel_r','gumbel_l','halfcauchy','halflogistic','halfnorm','halfgennorm','hypsecant','invgamma','invgauss','invweibull','johnsonsb','johnsonsu','kstwobign','laplace','levy','levy_l','logistic','loggamma','loglaplace','lognorm','lomax','maxwell','mielke','nakagami','ncx2','ncf','nct','norm','pareto','pearson3','powerlaw','powerlognorm','powernorm','rdist','reciprocal','rayleigh','rice','recipinvgauss','semicircular','t','triang','truncexpon','truncnorm','tukeylambda','uniform','vonmises','vonmises_line','wald','weibull_min','weibull_max']
+    # List of available Distributions for fitting in scipy
+    list_of_dists = ['alpha', 'anglit', 'arcsine', 'beta', 'betaprime', 'bradford', 'burr', 'burr12', 'cauchy', 'chi', 'chi2', 'cosine', 'dgamma', 'dweibull', 'expon', 'exponnorm', 'exponweib', 'exponpow', 'f', 'fatiguelife', 'fisk', 'foldcauchy', 'foldnorm', 'genlogistic', 'genpareto', 'gennorm', 'genexpon', 'genextreme', 'gausshyper', 'gamma', 'gengamma', 'genhalflogistic', 'gilbrat', 'gompertz', 'gumbel_r', 'gumbel_l', 'halfcauchy', 'halflogistic', 'halfnorm', 'halfgennorm', 'hypsecant',
+                     'invgamma', 'invgauss', 'invweibull', 'johnsonsb', 'johnsonsu', 'kstwobign', 'laplace', 'levy', 'levy_l', 'logistic', 'loggamma', 'loglaplace', 'lognorm', 'lomax', 'maxwell', 'mielke', 'nakagami', 'ncx2', 'ncf', 'nct', 'norm', 'pareto', 'pearson3', 'powerlaw', 'powerlognorm', 'powernorm', 'rdist', 'reciprocal', 'rayleigh', 'rice', 'recipinvgauss', 'semicircular', 't', 'triang', 'truncexpon', 'truncnorm', 'tukeylambda', 'uniform', 'vonmises', 'vonmises_line', 'wald', 'weibull_min', 'weibull_max']
+
+    results = []
+    for i in list_of_dists:
+        dist = getattr(stats, i)
+        param = dist.fit(MatrixColumn)
+        a = stats.kstest(MatrixColumn, i, args=param)
+        results.append((i, a[0], a[1]))
+
+    results.sort(key=lambda x: float(x[2]), reverse=True)
+    for j in results:
+        print("{}: statistic={}, pvalue={}".format(j[0], j[1], j[2]))
 
 
-	results = []
-	for i in list_of_dists:
-		dist = getattr(stats, i)
-		param = dist.fit(MatrixColumn)
-		a = stats.kstest(MatrixColumn, i, args=param)
-		results.append((i,a[0],a[1]))
-    
-    
-	results.sort(key=lambda x:float(x[2]), reverse=True)
-	for j in results:
-    		print("{}: statistic={}, pvalue={}".format(j[0], j[1], j[2]))
-
-
-
-##Read first CSV File
+# Read first CSV File
 dataFile1 = pd.read_csv("out_1.csv")
-##Read second CSV File
+# Read second CSV File
 dataFile2 = pd.read_csv("out_2.csv")
 
-#Add Zone columns
-dataFile1['zone']='1'
-dataFile2['zone']='2'
+# Add Zone columns
+dataFile1['zone'] = '1'
+dataFile2['zone'] = '2'
 
 FileZone = [dataFile1, dataFile2]
-listfeatures
+# listfeatures_distributions = [["mass","exponential"],["velocity","normal"],["time","exponential"]]
+listfeatures_distributions = [["mass", "exponential"]]
+sizeMonteCarloSim = 1_000_000
 
-#Mainloop for zone calculations
-for i in FileZone:
+
+# Mainloop for zone calculations
+for FileZone in FileZone:
     # calc fit dist features (for )
-    for x in listfeatures:
+    print(FileZone['zone'])
+    for featureDistribution in listfeatures_distributions:
         # calc fit dist features case when dist predefined
-        # calc monte carlo
-        # return calc monte carlo
-    
+        print(featureDistribution[0])
+        print(featureDistribution[1])
+        if (featureDistribution[1] == "exponential"):
+            explambda = mean(FileZone[featureDistribution[0]])
+            # generate sample
+            sample = exponential(explambda, sizeMonteCarloSim)
+            print(sample)
+            featureDistribution= np.append(featureDistribution,sample)
+            print(featureDistribution)
+        elif(featureDistribution[1] == "normal"):
+            sample = normal(size=sizeMonteCarloSim)
+            print(sample)
 
-
-
-
-
-
-
-
-
-    
+    # calc monte carlo
+    # return calc monte carlo
