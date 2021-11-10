18a39f246d81d339c410df9def92dd3ef0ca2b6b
patschue <84413011+patschue@users.noreply.github.com>
2021-11-10 18:56:22 +0100

    Update Merge Pandas

--- Merge Pandas.py
+++ Merge Pandas.py
@@ -12,6 +12,4 @@ Test1 = pd.DataFrame(np.array([[1, "a"], [3, "b"], [5, "c"]]), columns=['x', 'y'
 Test2 = pd.DataFrame(np.array([[2, "a"], [4, "b"], [6, "c"]]), columns=['x', 'y'])
 
 
-Result = Test1.merge(Test2, how="outer")
-# Result = Test1.merge(Test2, on = "x", how= "cross")
-# Result = Result.sort_values("x")
+Result = Test1.merge(Test2, how="outer", sort = True)
